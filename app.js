73470a1fd7be9150f14dc6ee306368ade8168391
kimdalusong <kimdalusong05@gmail.com>
2020-06-05 11:30:00 +0800

    initial commit

--- app.js
+++ app.js
@@ -130,10 +130,10 @@ app.post("/delete", function (req, res) {
 
 });
 
-let port = process.env.PORT;
+const port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 app.listen(port, function () {
     console.log("Server started on Port 3000");
-})
+});
